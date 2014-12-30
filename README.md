261f466f0a70b40771812de9cd9bf8eb5e434c93
Tyler Spadgenske <tyspadgens@gmail.com>
2014-12-29 19:30:48 -0600

    Added everything

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-thirtytwo-squared
+32 SQUARED
 =================
 
 1024 LEDs and a Raspberry Pi
+
+###Libraries
+The rpi-rgb-led-matrix library is included in this repository. It is under GPL 2.0 just like the rest of the code. 
+https://github.com/hzeller/rpi-rgb-led-matrix
---
+++ main.py
@@ -0,0 +1,68 @@
+#LED Controller
+#By Tyler Spadgenske
+
+import time, os, random
+
+class LEDS():
+    def __init__(self):
+        self.last_tile = ''
+        self.tiles = os.listdir('/home/pi/thirtytwo-squared/tiles')
+        self.tiles.remove('time.conf')
+        time.sleep(9)
+        self.lines = []
+        self.time = 0
+        self.mode = '0'
+        self.get_tile_time()
+        self.modes = []
+        self.scroll = ''
+        
+    def choose_tiles(self):
+        while True:
+            self.tile = random.choice(self.tiles)
+            if self.tile != self.last_tile:
+                self.last_tile = self.tile
+                break
+
+    def play_tiles(self):
+        while True:
+            self.choose_tiles()
+            for i in self.lines:
+                if self.tile in i:
+                    self.time = i[-2] + i[-1]
+            for i in self.modes:
+                if self.tile in i:
+                    self.mode = i[-1]
+
+            if self.mode == '1':
+                self.scroll = ''
+                self.mode = '1'
+            if self.mode == '0':
+                self.scroll = ' -m -1'
+                self.mode = '1'
+            if self.mode == '2':
+                self.scroll = ''
+                self.mode = '2'
+       
+            if self.time[0] == '0':
+                self.time = self.time[-1]
+
+            os.system('sudo rpi-rgb-led-matrix/./led-matrix -D ' + self.mode + ' /home/pi/thirtytwo-squared/tiles/' + self.tile + ' -t ' + self.time + '' + self.scroll)
+            time.sleep(3)
+
+    def get_tile_time(self):
+        self.file = open('/home/pi/thirtytwo-squared/tiles/time.conf', 'r')
+        for i in range(0, len(self.tiles) - 1):
+            self.lines.append(self.file.readline().rstrip())
+
+    def get_mode(self):
+        self.mode_file = open('/home/pi/thirtytwo-squared/tiles/mode.conf', 'r')
+        for i in range(0, len(self.tiles) - 1):
+            self.modes.append(self.mode_file.readline().rstrip())
+            
+if __name__ == '__main__':
+    display = LEDS()
+    display.get_mode()
+    while True:
+        display.play_tiles()
+        
+        
