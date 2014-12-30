7426776b5b943ae76d7dbe9523282435c2d4fa57
Tyler Spadgenske <tyspadgens@gmail.com>
2014-12-30 15:34:48 +0000

    Almost done with updater'

--- update.py
+++ update.py
@@ -7,33 +7,82 @@ class Update():
     def __init__(self):
         pass
 
-    def download(self):
-        pass
+    def download(self, files):
+        for i in files:
+            os.system('sudo wget -P /home/pi/thirtytwo-squared/tiles/ -4 https://github.com/spadgenske/thirtytwo-squared/raw/master/tiles/' + i.rstrip())
+            print 'Downloaded "' + i + '"'       
 
     def delete(self):
-        pass
+        old_files = os.listdir('/home/pi/thirtytwo-squared/tiles/')
+
+        #Delete the old files
+        for i in old_files:
+            os.remove('/home/pi/thirtytwo-squared/tiles/' + i)
+            print 'Deleted "' + i + '"'
 
     def check_connection(self):
+        #Open up a google server to check internet connection
         try:
             response=urllib2.urlopen('http://74.125.228.100',timeout=1)
             return True
         except urllib2.URLError as err: pass
-        return Falsepass
+        return False
 
     def check_for_update(self):
+        #See the current version of tiles
         old_tiles = open('/home/pi/thirtytwo-squared/static/tile-quanity.txt')
         num_of_old_tiles = old_tiles.readline()
         old_tiles.close()
-        print 'Old Tiles: ', num_of_old_tiles
 
+        #Remove file and get new one
         os.remove('/home/pi/thirtytwo-squared/static/tile-quanity.txt')
-        os.system('sudo wget -4 https://github.com/spadgenske/thirtytwo-squared/raw/master/static/tile-quanity.txt')
+        os.system('sudo wget -P /home/pi/thirtytwo-squared/static/ -4 https://github.com/spadgenske/thirtytwo-squared/raw/master/static/tile-quanity.txt static/tilequanity.txt')
         new_tiles = open('/home/pi/thirtytwo-squared/static/tile-quanity.txt')
         num_of_new_tiles = new_tiles.readline()
-        print 'New Tiles: ', num_of_new_tiles
+
+        #Compare versions to check for update
+        if num_of_new_tiles != num_of_old_tiles:
+            update_availible = True
+        else:
+            update_availible = False
+            
+        return update_availible
+                
+    def prepare_download(self):
+        os.remove('/home/pi/thirtytwo-squared/static/filenames.txt')
+        #Get all the filenames of the new/old tiles
+        os.system('sudo wget -P /home/pi/thirtytwo-squared/static/ -4 https://github.com/spadgenske/thirtytwo-squared/raw/master/static/filenames.txt static/filenames.txt')
+        filenames_file = open('/home/pi/thirtytwo-squared/static/filenames.txt')
+        new_filenames = filenames_file.readlines()
+        for i in new_filenames:
+            i = i.rstrip()
+
+        return new_filenames
+        
 
 if __name__ == '__main__':
     updater = Update()
     connection = updater.check_connection()
     if connection == True:
-        updater.check_for_update()
+        print '+++++++++++++++++++++++'
+        print 'Connected to Internet'
+        print '+++++++++++++++++++++++'
+        update_availible = updater.check_for_update()
+
+        if update_availible:
+            print '+++++++++++++++++++++++++++'
+            print 'New Update Availible'
+            print '+++++++++++++++++++++++++++'
+            new_files = updater.prepare_download()
+            updater.delete()
+            updater.download(new_files)
+            print 'Update Complete'
+        else:
+            print '+++++++++++++++++++++++++++'
+            print 'No Update Found'
+            print '+++++++++++++++++++++++++++'
+    else:
+        print '+++++++++++++++++++++++++++'
+        print 'Cannot Connect to Internet'
+        print '+++++++++++++++++++++++++++'
+
