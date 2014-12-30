d7dbc91c422d4d3bb1c0d09920f30966db24eb41
Tyler Spadgenske <tyspadgens@gmail.com>
2014-12-30 21:16:41 +0000

    No connection fix

--- update.py
+++ update.py
@@ -89,4 +89,5 @@ if __name__ == '__main__':
         print '+++++++++++++++++++++++++++'
         print 'Cannot Connect to Internet'
         print '+++++++++++++++++++++++++++'
+        time.sleep(37)
 
